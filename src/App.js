6dd0547a32aa68a181e4559c0920f42dd2e7d18a
jakovid <jacobk.dowdle@gmail.com>
2023-02-21 00:36:33 +0800

    changed to react and began to create the header

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import React from "react";
+import BuildHeader from "./components/header.js";
+
+function App() {
+  return (
+    <div className="App">
+      <BuildHeader />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/header.js
@@ -0,0 +1,38 @@
+import React from "react";
+import JacobDowdle from "../media/jacob-dowdle.png";
+import "../styles/header.css";
+
+export default function BuildHeader() {
+
+    const letters = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+
+    function HackerEffect(event) {
+        let iterations = 0;
+
+        const interval = setInterval(() => {
+            event.target.innerText = event.target.innerText.split("")
+            .map((letter, index) => {
+                if(index < iterations) {
+                    return event.target.dataset.value[index];
+                }
+                
+                return letters[Math.floor(Math.random() * 26)]
+            })
+            .join("");
+
+            if(iterations >= event.target.dataset.value.length) clearInterval(interval);
+
+            iterations += 1 / 3;
+        }, 30);
+    }
+
+    return(
+        <header className="header">
+            <div className="header-left">
+                <img className="header-img" src={JacobDowdle}/>
+                <h1 className="header-name" id="hacker-effect" onMouseOver={HackerEffect} data-value="JACOB DOWDLE">WHO IS THIS?</h1>
+            </div>
+            <div className="header-right"></div>
+        </header>
+    )
+}
