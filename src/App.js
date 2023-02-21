d65e0b7b50e9c65bdf1b463d32816db3a86f26d2
jakovid <jacobk.dowdle@gmail.com>
2023-02-21 11:27:25 +0800

    began to create the about me comp

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import React from "react";
 import BuildHeader from "./components/header.js";
 import BuildHero from "./components/hero.js";
+import BuildAbout from "./components/about.js";
 
 function App() {
   return (
     <div className="App">
       <BuildHeader />
       <BuildHero />
+      <BuildAbout />
     </div>
   );
 }
